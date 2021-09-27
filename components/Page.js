644254e7e249f9786a968ee2ef0eee0e8180bd68
Franek Marciniak <prostyfranek@protonmail.com>
2021-09-27 17:58:14 +0200

    added one day plan

--- components/Page.js
+++ components/Page.js
@@ -1,26 +1,34 @@
-import { createGlobalStyle } from "styled-components";
-
+import { createGlobalStyle } from 'styled-components'
+import Footer from './Footer'
 const GlobalStyles = createGlobalStyle`
-  html {
-    height: 100vh;
-    width: 100vw;
-    overflow-x: hidden;
-    font-family: "Azeret Mono", monospace;
-  }
-
   body {
   background-color: #2f3437;
   color: #fff;
+    height: 100vh;
+    width: 100vw;    
+    padding: 0;
+    margin: 0;
+    overflow-x: hidden;
   }
-  
-`;
+  *{
+      font-family: Azeret Mono, monospace;
+      
+  }
+  @media only screen and (max-width: 400px) {
+        body {
+            font-size: 13px;
+        }
+
+    }
+`
 function Page({ children }) {
-  return (
-    <>
-      <GlobalStyles />
-      {children}
-    </>
-  );
+    return (
+        <>
+            <GlobalStyles />
+            {children}
+            <Footer />
+        </>
+    )
 }
 
-export default Page;
+export default Page
