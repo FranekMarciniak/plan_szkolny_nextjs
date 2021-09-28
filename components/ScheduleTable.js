711ea6710af38280ba0b38900857c64f83f891aa
Franek Marciniak <prostyfranek@protonmail.com>
2021-09-28 11:22:59 +0200

    changed table for more reusablitity

--- components/ScheduleTable.js
+++ components/ScheduleTable.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import hours from '../utils/hours'
 const StyledColumn = styled.div`
-    width: 500px;
+    width: 460px;
     border: 1px solid #474c50;
     @media only screen and (max-width: 700px) {
         width: 90%;
@@ -22,22 +23,37 @@ const StyledColumnCell = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
     text-align: center;
 `
-// const StyledHours
+const StyledHours = styled.div`
+    width: 100%;
+    /* padding: 5px; */
+    display: flex;
+    justify-content: center;
+    background-color: #474c50;
+    p {
+        padding: 0;
+        margin: 0;
+    }
+`
 function ScheduleTable({ data, type }) {
     console.log(data)
     return (
         <>
             <StyledColumn>
-                {data.map((ele) => (
+                {data.map((ele, i) => (
                     <StyledColumnCell>
+                        <StyledHours>
+                            <p>{hours[i]}</p>
+                        </StyledHours>
                         {ele &&
                             (ele.flag === 'odwołane' ? (
                                 <del>{ele.title}</del>
                             ) : (
                                 <p>{ele.title}</p>
                             ))}
+                        {!ele && <p>-||-</p>}
                     </StyledColumnCell>
                 ))}
             </StyledColumn>
